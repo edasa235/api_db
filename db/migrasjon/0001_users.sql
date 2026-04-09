e0143772a0f3fc0572121320ee36f49fcdf7a2fc
edasa235 <edvardaslaksen130206@gmail.com>
2026-04-09 10:16:35 +0200

    feat: (updated db)

--- db/migrasjon/0001_users.sql
+++ db/migrasjon/0001_users.sql
@@ -1,10 +1,10 @@
-create table users(
-                      id uuid,
-                      username text not null,
-                      email text not null,
-                      created_at timestamptz default current_timestamp
+create table users (
+                       id uuid not null default gen_random_uuid(),
+                       username text not null,
+                       email text not null,
+                       created_at timestamptz default current_timestamp,
 
-                          constraint users_pkey primary key (id),
-                      constraint users_email_unique unique (email),
-                      constraint users_username_unique unique (username)
+                       constraint users_pkey primary key (id),
+                       constraint users_email_unique unique (email),
+                       constraint users_username_unique unique (username)
 );
--- db/migrasjon/0002_tasks.sql
+++ db/migrasjon/0002_tasks.sql
@@ -1,5 +1,5 @@
 create table tasks (
-                       id uuid,
+                       id uuid not null default gen_random_uuid(),
                        title text not null,
                        description text null,
                        status text not null,
@@ -7,9 +7,10 @@ create table tasks (
                        created_at timestamptz default current_timestamp,
                        completed_at timestamptz null,
 
-                       constraint status_check check (status in ('BACKLOG', 'READY', 'IN_PROGRESS', 'IN_REVIEW', 'IN_TESTING', 'COMPLETED')),
                        constraint tasks_pkey primary key (id),
-                       constraint tasks_assignee_id_fk foreign key (assignee_id) references users(id),
-    constraint INDEX tasks_assignee_id_idx (assignee_id),
-    constraint INDEX tasks_status_idx (status)
+                       constraint tasks_assignee_id_fk foreign key (assignee_id) references users(id) on delete restrict,
+                       constraint status_check check (status in ('BACKLOG', 'READY', 'IN_PROGRESS', 'IN_REVIEW', 'IN_TESTING', 'COMPLETED'))
 );
+
+create index tasks_assignee_id_idx on tasks(assignee_id);
+create index tasks_status_idx on tasks(status);
