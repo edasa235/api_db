1f78fc6ff815c44c8257a26ae6034f10aa0e78af
edasa235 <edvardaslaksen130206@gmail.com>
2026-04-09 12:32:50 +0200

    feat: (updated db) refactor db

--- db/migrasjon/0001_users.sql
+++ db/migrasjon/0001_users.sql
@@ -1,11 +1,13 @@
 create extension if not exists pgcrypto;
-create table users (
-                       id uuid not null default gen_random_uuid(),
-                       username text not null unique,
-                       email text not null,
-                       created_at timestamptz default current_timestamp,
 
-                       constraint users_pkey primary key (id),
-                       constraint users_email_unique unique (email),
-                       constraint users_username_unique unique (username)
+create table users
+(
+    id         uuid        not null default gen_random_uuid(),
+    username   text        not null,
+    email      text        not null,
+    created_at timestamptz not null default current_timestamp,
+
+    constraint users_pkey primary key (id),
+    constraint users_email_unique unique (email),
+    constraint users_username_unique unique (username)
 );
--- db/migrasjon/0002_tasks.sql
+++ db/migrasjon/0002_tasks.sql
@@ -1,21 +1,24 @@
 BEGIN;
 create extension if not exists pgcrypto;
+create table tasks
+(
+    id           uuid        not null default gen_random_uuid(),
+    title        text        not null,
+    description  text        null,
+    status       text        not null,
+    assignee_id  uuid        null,
+    created_at   timestamptz not null default current_timestamp,
+    completed_at timestamptz null,
 
-create table tasks (
-                       id uuid not null default gen_random_uuid(),
-                       title text not null,
-                       description text null,
-                       status text not null,
-                       assignee_id uuid not null,
-                       created_at timestamptz default current_timestamp,
-                       completed_at timestamptz null,
-
-                       constraint tasks_pkey primary key (id),
-                       constraint tasks_assignee_id_fk foreign key (assignee_id) references users(id) on delete restrict,
-                       constraint status_check check (status in ('BACKLOG', 'READY', 'IN_PROGRESS', 'IN_REVIEW', 'IN_TESTING', 'COMPLETED')),
-                       constraint completed_at_check check ((status = 'COMPLETED' and completed_at is not null) or (status != 'COMPLETED' and completed_at is null))
-                   );
-
-create index tasks_assignee_id_idx on tasks(assignee_id);
-create index tasks_status_idx on tasks(status);
-COMMIT ;
+    constraint tasks_pkey primary key (id),
+    constraint tasks_assignee_id_fk foreign key (assignee_id) references users (id) on delete set null,
+    constraint tasks_status_check check (status in
+                                         ('BACKLOG', 'READY', 'IN_PROGRESS', 'IN_REVIEW', 'IN_TESTING', 'COMPLETED')),
+    constraint tasks_completed_at_check check (
+        (status = 'COMPLETED' and completed_at is not null)
+            or (status != 'COMPLETED' and completed_at is null)
+        )
+);
+create index tasks_assignee_id_idx on tasks (assignee_id);
+create index tasks_status_idx on tasks (status);
+COMMIT;
